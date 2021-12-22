b2e5963a70e498fe8bf870d9bdbad4ec1af4bd8f
Louis Bettens <louis@bettens.info>
2021-12-22 18:52:04 +0100

    fix

--- template/lib/compat.nix
+++ template/lib/compat.nix
@@ -1,5 +1,5 @@
 let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  lock = builtins.fromJSON (builtins.readFile ../flake.lock);
   inherit (lock.nodes.flake-compat.locked) rev narHash;
   flake-compat = fetchTarball {
     url = "https://github.com/edolstra/flake-compat/archive/${rev}.tar.gz";
